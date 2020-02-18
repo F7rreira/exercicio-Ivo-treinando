1e0a1c1b780bc22f4c7ef4fbf037f63c5cdd53c8
Treinamento <vini.dejesus98@hotmail.com>
2020-02-18 19:30:31 -0300

    exercicio

---
+++ src/Exercicio7/Veiculo.java
@@ -0,0 +1,12 @@
+package Exercicio7;
+
+public class Veiculo{
+    private String nome;
+    private double numeroDeRodas;
+    private double capacidadeTanque;
+    private double consumo;
+
+
+
+
+}
